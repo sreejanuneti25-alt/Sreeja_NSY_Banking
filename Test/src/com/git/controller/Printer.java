9cca64ee1c5fb0b4f1c9cfe355155b2f14d56322
sreeja <sreejanuneti25@gmail.com>
2026-05-19 11:46:21 +0000

    changed made

---
+++ Test/src/com/git/controller/Printer.java
@@ -0,0 +1,10 @@
+package com.git.controller;
+
+public class Printer {
+	String printernumber(String printername) {
+	return "The printer number is "+printername;
+}
+void print(String printername) {
+	System.out.println("The printer name is "+printername);
+}
+}
